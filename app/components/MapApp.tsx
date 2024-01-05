2fecdcf1e58739da6f63251c9d8a2a7c624dec88
Renato Rodrigues <renato072002@gmail.com>
2024-01-05 20:04:47 -0300

    second-commit

--- app/components/MapApp.tsx
+++ app/components/MapApp.tsx
@@ -1,12 +1,25 @@
 'use client'
 
-import React from 'react'
+import React, { useState } from 'react'
 import { MapContainer, TileLayer, Marker, Popup } from 'react-leaflet'
 import "leaflet/dist/leaflet.css"
 import { Icon } from 'leaflet'
-import Image from 'next/image'
+import eventsData from './HistoricalEvents'
+
+export interface HistoricalEvent{
+    id:number,
+    title: string,
+    description: string,
+    position: [number, number],
+    category: string;
+};
 
 const defaultPosition: [number, number] = [51.505, -0.09]
+
+const emptyStar = <i className='fa-regular fa-star'></i>
+const fullStar = <i className='fa-solid fa-star' style={{
+    color: "#fdc401",
+}}></i>
 
 function MapApp() {
     const icon: Icon = new Icon({
@@ -15,20 +28,72 @@ function MapApp() {
         iconAnchor: [12, 41],
     });
 
+    const [activeEvent, setActiveEvent] = useState<HistoricalEvent | null>(null);
+    const[favorites, setFavorites] = useState<number[]>(() => {
+        const savedFavorites = localStorage.getItem("Favorites");
+        return savedFavorites ? JSON.parse(savedFavorites) : []
+    });
+
+    const handleFavoriteClick = (eventId: number) => {
+        let updatedFavorites = favorites.filter((id) => id !== eventId);
+    
+        if (!favorites.includes(eventId)) {
+          updatedFavorites = [eventId, ...updatedFavorites];
+        }
+    
+        setFavorites(updatedFavorites);
+        localStorage.setItem("favorites", JSON.stringify(updatedFavorites));
+      };
+
   return (
     <div className='content'>
       <div className='flex flex-col w-full h-full'>
             <div className='h-12'></div>
           <MapContainer center={defaultPosition} zoom={13} className='map-container'>
           <TileLayer url='https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png' />
-          <Marker position={defaultPosition} icon={icon}>
-            
-            <Popup >
-              A pretty CSS3. <br /> Easily blala
-            </Popup>
-          </Marker>
-        </MapContainer>
-        
+          {
+            eventsData.map((event) =>{
+                return <Marker key={event.id} position={event.position} icon={icon} eventHandlers={{
+                    click: () => {
+                        setActiveEvent(event)
+                    }
+                }}
+                />
+            })
+          }
+          {activeEvent && (
+            <Popup position={activeEvent.position}>
+            <div className='popup-inner'>
+                <h2 className='popup-inner_title'>
+                    {activeEvent.title}
+                </h2>
+            </div>
+            <p className='popup-inner_description'>
+                {activeEvent.description}
+            </p>
+            <button className='popup-inner_button'
+            onClick={() => handleFavoriteClick(activeEvent.id)}
+            >
+            {favorites.includes(activeEvent.id) ? (
+                  <span>{fullStar} Desfavoritar</span>
+                ) : (
+                  <span>{emptyStar} Favoritar</span>
+                )}
+            </button>
+        </Popup>
+          )}
+        </MapContainer>    
+      </div>
+
+      <div className="liked-events">
+        <h2 className="liked-events_title">
+            <i className='fa-solid fa-star'></i>Eventos Favoritos
+        </h2>
+        <ul>
+            {favorites.map((event)=> {
+                return <li key={event.id}></li>
+            })}
+        </ul>
       </div>
     </div>
   )
