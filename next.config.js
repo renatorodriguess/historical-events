8ec2ebb9ec0af7fea3e57e73bb60d6b7aefea1fe
Renato Rodrigues <renato072002@gmail.com>
2024-01-06 00:27:18 -0300

    Update next.config.js

--- next.config.js
+++ next.config.js
@@ -1,6 +1,6 @@
 // next.config.js
 module.exports = {
-  async exportPathMap() {
+  async generateStaticParams() {
     // Sua lógica de exportação de mapa aqui
     return {
       '/': { page: '/' },
